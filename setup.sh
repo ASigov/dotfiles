17fe011efe99d02b1009c1984b80a41b4d9f1a8c
Alexey Sigov <opminded@gmail.com>
2025-08-17 17:07:24 +0300

    Add starship file

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/.config/fish/fish_plugins ~/.config/fish/fish_plugins
+ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
