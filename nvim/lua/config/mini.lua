b828db344b61cc1522684176f4b316f5ef5ce6ca
Alexey Sigov <opminded@gmail.com>
2025-08-21 21:35:51 +0300

    Enable mini.diff, add autotag plugin
    Add keymaps, cleanup existing ones.

--- nvim/lua/config/mini.lua
+++ nvim/lua/config/mini.lua
@@ -2,6 +2,11 @@ require("mini.notify").setup()
 vim.notify = require("mini.notify").make_notify()
 
 require("mini.icons").setup()
+require("mini.diff").setup()
 require("mini.statusline").setup()
 require("mini.pairs").setup()
 require("mini.surround").setup()
+
+vim.keymap.set("n", "<leader>d", function()
+    MiniDiff.toggle_overlay(vim.api.nvim_get_current_buf())
+end, { desc = "Toggle [D]iff overlay" })
--- nvim/lua/config/nvim-treesitter.lua
+++ nvim/lua/config/nvim-treesitter.lua
@@ -15,6 +15,7 @@ MiniDeps.add({
     checkout = "main",
 })
 MiniDeps.add("nvim-treesitter/nvim-treesitter-context")
+MiniDeps.add("windwp/nvim-ts-autotag")
 
 require("nvim-treesitter").install({
     "css",
@@ -42,11 +43,12 @@ require("nvim-treesitter-textobjects").setup({
         selection_modes = {
             ["@parameter.outer"] = "v", -- charwise
             ["@function.outer"] = "V", -- linewise
-            ["@class.outer"] = "<c-v>", -- blockwise
+            ["@class.outer"] = "V", -- linewise
         },
         include_surrounding_whitespace = false,
     },
 })
+require("nvim-ts-autotag").setup()
 
 vim.keymap.set({ "x", "o" }, "af", function()
     require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
@@ -60,3 +62,29 @@ end)
 vim.keymap.set({ "x", "o" }, "ic", function()
     require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
 end)
+
+vim.keymap.set("n", "<A-l>", function()
+    require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
+end)
+vim.keymap.set("n", "<A-h>", function()
+    require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
+end)
+vim.keymap.set("n", "<A-j>", function()
+    require("nvim-treesitter-textobjects.swap").swap_next("@function.outer")
+end)
+vim.keymap.set("n", "<A-k>", function()
+    require("nvim-treesitter-textobjects.swap").swap_previous("@function.outer")
+end)
+
+-- vim.keymap.set("n", "<C-l>", function()
+--     require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.inner", "textobjects")
+-- end)
+-- vim.keymap.set("n", "<C-h>", function()
+--     require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.inner", "textobjects")
+-- end)
+-- vim.keymap.set("n", "<C-j>", function()
+--     require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+-- end)
+-- vim.keymap.set("n", "<C-k>", function()
+--     require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+-- end)
