4843392d93d3600e94a8cf0d67cea140534315c6
Alexey Sigov <opminded@gmail.com>
2025-10-22 19:32:37 +0300

    Update mini.nvim to reflect new org name
    Use MiniMax config files as an example for making changes.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,25 @@
--- Clone 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
-local path_package = vim.fn.stdpath("data") .. "/site/"
-local mini_path = path_package .. "pack/deps/start/mini.nvim"
+-- Bootstrap 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
+local mini_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/mini.nvim"
 if not vim.loop.fs_stat(mini_path) then
     vim.cmd('echo "Installing `mini.nvim`" | redraw')
-    local clone_cmd = {
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/echasnovski/mini.nvim",
-        mini_path,
-    }
+    local origin = "https://github.com/nvim-mini/mini.nvim"
+    local clone_cmd = { "git", "clone", "--filter=blob:none", origin, mini_path }
     vim.fn.system(clone_cmd)
     vim.cmd("packadd mini.nvim | helptags ALL")
     vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
 
--- Set up 'mini.deps' (customize to your liking)
-require("mini.deps").setup({ path = { package = path_package } })
+-- Plugin manager. Set up immediately for `now()`/`later()` helpers.
+-- Example usage:
+-- - `MiniDeps.add('...')` - use inside config to add a plugin
+-- - `:DepsUpdate` - update all plugins
+-- - `:DepsSnapSave` - save a snapshot of currently active plugins
+--
+-- See also:
+-- - `:h MiniDeps-overview` - how to use it
+-- - `:h MiniDeps-commands` - all available commands
+-- - 'plugin/30_mini.lua' - more details about 'mini.nvim' in general
+require("mini.deps").setup()
 
 vim.g.mapleader = " "
 
