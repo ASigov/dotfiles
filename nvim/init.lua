1bae050cbb59b1f254b10ab6687bbcca3d462033
Alexey Sigov <opminded@gmail.com>
2025-11-18 21:40:39 +0300

    Integrate options from MiniMax
    Bring 10_options.lua from MiniMax, make small tweaks based on old
    options.
    Add more stuff to init.lua from MiniMax.
    Remove old diagnostics config since it now lives inside 10_options.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
+-- ┌────────────────────┐
+-- │ Welcome to MiniMax │
+-- └────────────────────┘
+--
+-- This is a config designed to mostly use MINI. It provides out of the box
+-- a stable, polished, and feature rich Neovim experience. Its structure:
+--
 -- ├ init.lua          Initial (this) file executed during startup
 -- ├ plugin/           Files automatically sourced during startup
 -- ├── 10_options.lua  Built-in Neovim behavior
@@ -9,6 +16,31 @@
 -- ├── ftplugin/       Files for filetype behavior (has demo file)
 -- ├── lsp/            Language server configurations (has demo file)
 -- ├── snippets/       Higher priority snippet files (has demo file)
+--
+-- Config files are meant to be read, preferably inside a Neovim instance running
+-- this config and opened at its root. This will help you better understand your
+-- setup. Start with this file. Any order is possible, prefer the one listed above.
+-- Ways of navigating your config:
+-- - `<Space>` + `e` + (one of) `iokmp` - edit 'init.lua' or 'plugin/' files.
+-- - Inside config directory: `<Space>ff` (picker) or `<Space>ed` (explorer)
+-- - Navigate existing buffers with `[b`, `]b`, or `<Space>fb`.
+--
+-- Config files are also meant to be customized. Initially it is a baseline of
+-- a working config based on MINI. Modify it to make it yours. Some approaches:
+-- - Modify already existing files in a way that keeps them consistent.
+-- - Add new files in a way that keeps config consistent.
+--   Usually inside 'plugin/' or 'after/'.
+--
+-- Documentation comments like this can be found throughout the config.
+-- Common conventions:
+--
+-- - See `:h key-notation` for key notation used.
+-- - `:h xxx` means "documentation of helptag xxx". Either type text directly
+--   followed by Enter or type `<Space>fh` to open a helptag fuzzy picker.
+-- - "Type `<Space>fh`" means "press <Space>, followed by f, followed by h".
+--   Unless said otherwise, it assumes that Normal mode is current.
+-- - "See 'path/to/file'" means see open file at described path and read it.
+-- - `:SomeCommand ...` or `:lua ...` means execute mentioned command.
 
 -- Bootstrap 'mini.nvim' manually in a way that it gets managed by 'mini.deps'
 local mini_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/mini.nvim"
@@ -32,3 +64,24 @@ end
 -- - `:h MiniDeps-commands` - all available commands
 -- - 'plugin/30_mini.lua' - more details about 'mini.nvim' in general
 require("mini.deps").setup()
+
+-- Define config table to be able to pass data between scripts
+_G.Config = {}
+
+-- Define custom autocommand group and helper to create an autocommand.
+-- Autocommands are Neovim's way to define actions that are executed on events
+-- (like creating a buffer, setting an option, etc.).
+--
+-- See also:
+-- - `:h autocommand`
+-- - `:h nvim_create_augroup()`
+-- - `:h nvim_create_autocmd()`
+local gr = vim.api.nvim_create_augroup("custom-config", {})
+_G.Config.new_autocmd = function(event, pattern, callback, desc)
+    local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+    vim.api.nvim_create_autocmd(event, opts)
+end
+
+-- Some plugins and 'mini.nvim' modules only need setup during startup if Neovim
+-- is started like `nvim -- path/to/file`, otherwise delaying setup is fine
+_G.Config.now_if_args = vim.fn.argc(-1) > 0 and MiniDeps.now or MiniDeps.later
--- nvim/plugin/10_options.lua
+++ nvim/plugin/10_options.lua
@@ -1,41 +1,146 @@
+-- ┌──────────────────────────┐
+-- │ Built-in Neovim behavior │
+-- └──────────────────────────┘
+--
+-- This file defines Neovim's built-in behavior. The goal is to improve overall
+-- usability in a way that works best with MINI.
+--
+-- Here `vim.o.xxx = value` sets default value of option `xxx` to `value`.
+-- See `:h 'xxx'` (replace `xxx` with actual option name).
+--
+-- Option values can be customized on per buffer or window basis.
+-- See 'after/ftplugin/' for common example.
+
+-- stylua: ignore start
+-- The next part (until `-- stylua: ignore end`) is aligned manually for easier
+-- reading. Consider preserving this or remove `-- stylua` lines to autoformat.
+
 -- General ====================================================================
 vim.g.mapleader = " " -- Use `<Space>` as <Leader> key
 
--- Search options
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.o.undofile = true -- Enable persistent undo
 
--- Displaying text
-vim.o.scrolloff = 10
-vim.o.wrap = false
-vim.o.list = true
-vim.o.listchars = "tab: ,trail:·,nbsp:␣"
-vim.o.number = true
-vim.o.relativenumber = true
+-- UI =========================================================================
+vim.o.breakindent    = true       -- Indent wrapped lines to match line start
+vim.o.breakindentopt = "list:-1"  -- Add padding for lists (if 'wrap' is set)
+vim.o.cursorline     = true       -- Enable current line highlighting
+vim.o.linebreak      = true       -- Wrap lines at 'breakat' (if 'wrap' is set)
+vim.o.list           = true       -- Show helpful text indicators (see 'listchars' below)
+vim.o.number         = true       -- Show line number
+vim.o.pumheight      = 10         -- Make popup menu smaller
+vim.o.relativenumber = true       -- Make line numbers relative
+vim.o.scrolloff      = 10         -- Keep number of lines above and below cursor
+vim.o.shortmess      = "CFOSWaco" -- Disable some built-in completion messages
+vim.o.showmode       = false      -- Don't show mode in command line
+vim.o.signcolumn     = "yes"      -- Always show signcolumn (less flicker)
+vim.o.splitbelow     = true       -- Horizontal splits will be below
+vim.o.splitkeep      = "screen"   -- Reduce scroll during window split
+vim.o.splitright     = true       -- Vertical splits will be to the right
+vim.o.winborder      = "single"   -- Use border in floating windows
+vim.o.wrap           = false      -- Don't visually wrap lines (toggle with \w)
 
--- Highlighting
-vim.o.cursorline = true
+vim.o.cursorlineopt = "screenline,number" -- Show cursor line per screen line
 
--- Spelling
--- vim.o.spell = true
+-- Special UI symbols. More is set via 'mini.basics' later.
+vim.o.fillchars = "eob: ,fold:╌"
+vim.o.listchars = "extends:…,nbsp:␣,precedes:…,tab: ,trail:·"
 
--- Multiple windows
-vim.o.splitright = true
-vim.o.splitbelow = true
+-- Folds (see `:h fold-commands`, `:h zM`, `:h zR`, `:h zA`, `:h zj`)
+vim.o.foldlevel   = 10       -- Fold nothing by default; set to 0 or 1 to fold
+vim.o.foldmethod  = "indent" -- Fold based on indent level
+vim.o.foldnestmax = 10       -- Limit number of fold levels
+vim.o.foldtext    = ""       -- Show text under fold with its highlighting
 
--- Messages and info
-vim.o.confirm = true
-vim.o.showmode = false
+-- Editing ====================================================================
+vim.o.autoindent    = true     -- Use auto indent
+vim.o.confirm       = true     -- Raise confirmation dialog instead of failing
+vim.o.expandtab     = true     -- Convert tabs to spaces
+vim.o.formatoptions = "rqnl1j" -- Improve commend editing
+vim.o.ignorecase    = true     -- Ignore case during search
+vim.o.incsearch     = true     -- Show search matches while typing
+vim.o.infercase     = true     -- Infer case in built-in completion
+vim.o.shiftwidth    = 4        -- Use this number of spaces for indentation
+vim.o.showmatch     = true     -- Briefly jump to matching bracket if insert one
+vim.o.smartcase     = true     -- Respect case if search pattern has upper case
+vim.o.smartindent   = true     -- Make indenting smart
+vim.o.softtabstop   = 4        -- Show tab as this number of spaces
+vim.o.spell         = false    -- Disable built-in spell checking
+vim.o.spelloptions  = "camel"  -- Treat camelCase word parts as separate words
+vim.o.virtualedit   = "block"  -- Allow going past end of line in blockwise mod
 
--- Editing text
-vim.o.showmatch = true
+vim.o.iskeyword = "@,48-57,_,192-255,-" -- Treat dash as `word` textobject part
 
--- Tabs and indenting
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-vim.o.expandtab = true
-vim.o.smartindent = true
+-- Pattern for a start of numbered list (used in `gw`). This reads as
+-- "Start of list item is: at least one special character (digit, -, +, *)
+-- possibly followed by punctuation (. or `)`) followed by at least one space".
+vim.o.formatlistpat = [[^\s*[0-9\-\+\*]\+[\.\)]*\s\+]]
 
--- Various
-vim.o.signcolumn = "yes"
-vim.o.winborder = "rounded"
+-- Built-in completion
+vim.o.complete    = ".,w,b,kspell"                  -- Use less sources
+vim.o.completeopt = "menuone,noselect,fuzzy,nosort" -- Use custom behavior
+
+-- Autocommands ===============================================================
+
+-- Don't auto-wrap comments and don't insert comment leader after hitting 'o'.
+-- Do on `FileType` to always override these changes from filetype plugins.
+local f = function() vim.cmd("setlocal formatoptions-=c formatoptions-=o") end
+_G.Config.new_autocmd("FileType", nil, f, "Proper 'formatoptions'")
+
+-- There are other autocommands created by 'mini.basics'. See 'plugin/30_mini.lua'.
+
+-- Diagnostics ================================================================
+
+-- Neovim has built-in support for showing diagnostic messages. This configures
+-- a more conservative display while still being useful.
+-- See `:h vim.diagnostic` and `:h vim.diagnostic.config()`.
+local diagnostic_opts = {
+    -- Show all diagnostics as underline (for their messages type `<Leader>ld` or `<C-W><C-D>`)
+    underline = {
+        severity = {
+            min = vim.diagnostic.severity.HINT,
+            max = vim.diagnostic.severity.ERROR,
+        }
+    },
+
+    -- Show more details immediately for errors on the current line
+    virtual_text = {
+        severity = {
+            min = vim.diagnostic.severity.ERROR,
+            max = vim.diagnostic.severity.ERROR,
+        },
+        current_line = true,
+        source = "if_many",
+        spacing = 1,
+    },
+    virtual_lines = false,
+
+    -- Show signs on top of any other sign, but only for warnings and errors
+    signs = {
+        severity = {
+            min = vim.diagnostic.severity.WARN,
+            max = vim.diagnostic.severity.ERROR,
+        },
+        priority = 9999,
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+    },
+
+    float = { -- <C-W><C-D> to show diagnostic float
+        source = "if_many",
+        border = "single",
+    },
+
+    -- Update diagnostics only when leaving Insert mode
+    update_in_insert = false,
+
+    -- Sort diagnostics by severity
+    severity_sort = true,
+}
+
+-- Use `later()` to avoid sourcing `vim.diagnostic` on startup
+MiniDeps.later(function() vim.diagnostic.config(diagnostic_opts) end)
+-- stylua: ignore end
