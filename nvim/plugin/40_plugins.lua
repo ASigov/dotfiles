828ec6ea2e7c3518fd79143df5832352653b46d7
Alexey Sigov <opminded@gmail.com>
2026-03-08 14:04:20 +0300

    Update treesitter config - main is now default branch

--- nvim/plugin/40_plugins.lua
+++ nvim/plugin/40_plugins.lua
@@ -31,8 +31,6 @@ local now_if_args = _G.Config.now_if_args
 now_if_args(function()
     add({
         source = 'nvim-treesitter/nvim-treesitter',
-        -- Use `main` branch since `master` branch is frozen, yet still default
-        checkout = 'main',
         -- Update tree-sitter parser after plugin is updated
         hooks = { post_checkout = function() vim.cmd('TSUpdate') end },
     })
